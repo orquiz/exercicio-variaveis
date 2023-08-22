e916dadbec686bd983363701add9430ec65f7bfb
orquiz <142925160+orquiz@users.noreply.github.com>
2023-08-22 17:18:51 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,38 @@
+/*
+* Luiza Orquiz
+*
+*/
+
+
+
+
+let a= 10
+let b = 20
+let c;
+console.log(b)
+console.log(a,b)
+c = a
+a = b
+b = c
+console.log (a,b);
+//*
+
+let nome;
+let idade;
+console.log(nome,idade);
+console.log(typeof nome);
+console.log(typeof idade);                                         
+nome = prompt("qual seu nome");
+console.log("ola", nome, "voce tem", idade, "anos");
+let variavel;
+variavel = prompt ("voce esta de roupa rosa hoje");
+console.log("voce esta de roupa rosa hoje", variavel);
+let variavel2 = prompt ("variavelSim + variavelNao") ;
+console.log(variavel2);
+let variavel3 = prompt ("porque voce nao esta de roupa verde hoje");
+console.log(variavel3);
+
+
+
+
+
